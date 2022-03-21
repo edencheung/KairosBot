202422fe4d61b6cc44986f2a489eb96a04af27d0
Eden <cheungedenhk@gmail.com>
2022-03-21 20:00:37 +0800

    fix listing cite stat posting

--- src/listing-manager.ts
+++ src/listing-manager.ts
@@ -1,7 +1,7 @@
 import { Api as TopggApi } from "@top-gg/sdk";
 import { InfinityBots } from "infinity-bots";
 import { bot, config } from ".";
-import fetch from "node-fetch";
+import axios from "axios";
 const topggApi = new TopggApi(config.TOP_GG_TOKEN);
 
 const infinityBotsApi = new InfinityBots(config.INFINITY_BOTS_TOKEN);
@@ -23,43 +23,43 @@ export async function postBotStats() {
     shards: 1,
   });
   // discord bot list
-  // await fetch(`https://discordbotlist.com/api/v1/bots/${bot.user.id}/stats`, {
-  //   method: "POST",
-  //   headers: {
-  //     Authorization: config.DISCORD_BOT_LIST_TOKEN,
-  //   },
-  //   body: JSON.stringify({
-  //     users: bot.users.cache.size,
-  //     guilds: bot.guilds.cache.size,
-  //     voice_connections: 0,
-  //   }),
-  // });
-  //discord extreme list
-  // console.log(
-  //   await fetch(
-  //     `https://api.discordextremelist.xyz/v2/bot/${bot.user.id}/stats`,
-  //     {
-  //       method: "POST",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: config.DISCORD_EXTREME_LIST_TOKEN,
-  //       },
-  //       body: JSON.stringify({
-  //         guildCount: bot.guilds.cache.size,
-  //       }),
-  //     }
-  //   )
-  // );
+  await axios
+    .post(
+      `https://discordbotlist.com/api/v1/bots/950382032620503091/stats`,
+      {
+        users: bot.users.cache.size,
+        guilds: bot.guilds.cache.size,
+        voice_connections: 0,
+      },
+      {
+        headers: {
+          Authorization: config.DISCORD_BOT_LIST_TOKEN,
+        },
+      }
+    )
+    .then(console.log);
+  // discord extreme list
+  await axios.post(
+    `https://api.discordextremelist.xyz/v2/bot/950382032620503091/stats`,
+    {
+      guildCount: bot.guilds.cache.size,
+    },
+    {
+      headers: {
+        Authorization: config.DISCORD_EXTREME_LIST_TOKEN,
+      },
+    }
+  );
   //discord bots
-  // console.log(
-  //   await fetch(`https://discord.bots.gg/api/v1/bots/${bot.user.id}/stats`, {
-  //     method: "POST",
-  //     headers: {
-  //       Authorization: config.DISCORD_BOTS_TOKEN,
-  //     },
-  //     body: JSON.stringify({
-  //       guildCount: bot.guilds.cache.size,
-  //     }),
-  //   })
-  // );
+  await axios.post(
+    `https://discord.bots.gg/api/v1/bots/950382032620503091/stats`,
+    {
+      guildCount: bot.guilds.cache.size,
+    },
+    {
+      headers: {
+        Authorization: config.DISCORD_BOTS_TOKEN,
+      },
+    }
+  );
 }
